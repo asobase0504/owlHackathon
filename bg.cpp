e055e63dc16fd28e326f8d5d57cc293e2f60cd96
鰤谷光太 <211063@st.yoshida-g.ac.jp>
2022-09-30 10:24:52 +0900

    テクスチャの追加

--- bg.cpp
+++ bg.cpp
@@ -86,10 +86,9 @@ CBg *CBg::Create()
 	if (pObject != nullptr)
 	{
 		pObject->Init();
-		pObject->SetSize(D3DXVECTOR3(640.0f, 360.0f,0.0f));
 		pObject->SetPos(D3DXVECTOR3(0.0f, 0.0f,10200.0f));
-		pObject->SetCollar(D3DXCOLOR(1.0f, 1.0f, 1.0f, 0.5f));
-
+		pObject->SetSize(D3DXVECTOR3(640.0f, 360.0f,0.0f));
+		pObject->SetCollar(D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f));
 	}
 
 	return pObject;
